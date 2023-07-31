d188e513342fc3e60340224843175586d8b11ada
christyansegecs <christyansegecs@hotmail.com>
2023-07-30 23:10:06 -0300

    fix vite config

--- packages/docs/vite.config.js
+++ packages/docs/vite.config.js
@@ -6,13 +6,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: [
-        {
-            find: 'src',
-            replacement: path.resolve(__dirname, './src'),
-        },  
-    ]
-  }
 })
-
