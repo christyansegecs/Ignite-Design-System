aa382c6e458f5f8940d884cbb234050cc94e9f61
christyansegecs <christyansegecs@hotmail.com>
2023-08-01 11:21:12 -0300

    fix vite config

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,26 +1,29 @@
-module.exports = {
-  "stories": [
-    "../src/pages/**/*.stories.mdx",
-    "../src/stories/**/*.stories.tsx"
+/** @type { import('@storybook/react-vite').StorybookConfig } */
+const config = {
+  stories: [
+    "../src/pages/**/*.stories.mdx", 
+    "../src/stories/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-a11y",
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-vite"
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
   },
-  "features": {
-    "storyStoreV7": true
+  docs: {
+    autodocs: "tag",
   },
-  "viteFinal": (config, {configType}) => {
+  viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/Ignite-Design-System'
+      config.base = '/design-system-reactjs-ignite2022/'
     }
-
-    return config;
+    
+    return config
   }
-}
+
+};
+export default config;
